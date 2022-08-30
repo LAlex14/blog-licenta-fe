7547a65df527964b03a50295c15e380ea449caee
Lesan Alexandru <alexandru.lesan@binarcode.com>
2022-08-30 16:28:57 +0300

    BLF-1: updates

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
             to="/blogs"
           >
             <img alt="Workflow" class="h-8 w-auto"
-                 src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"/>
+                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg">
           </router-link>
           <div class="hidden md:ml-6 md:flex md:space-x-8">
             <div
--- src/modules/common/utils/linkUtils.js
+++ src/modules/common/utils/linkUtils.js
@@ -1,4 +1,4 @@
-export const DEFAULT_API_URL = 'http://127.0.0.1:8000';
+export const DEFAULT_API_URL = 'http://blog-licenta-be.test';
 export const LOCAL_HOST = 'localhost';
 export const STORAGE_URL = DEFAULT_API_URL + '/storage/';
 export const DEFAULT_USER_AVATAR = 'https://www.weact.org/wp-content/uploads/2016/10/Blank-profile.png';
